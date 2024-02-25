9f66389019da57204a786e0537bd55b50b334b50
Alex Beng <599267065@qq.com>
2024-02-26 00:38:35 +0800

    add print dataset stats

---
+++ print_dataset_stats.py
@@ -0,0 +1,18 @@
+# 打印出dataset的norm stats
+# 单纯好奇均值和标准差是多少
+
+import pickle
+
+import IPython
+e = IPython.embed
+
+if __name__ == "__main__":
+    pkl_path = './models/dataset_stats.pkl'
+    with open(pkl_path, 'rb') as f:
+        stats = pickle.load(f)
+        # e()
+        for k in stats:
+            print(k)
+            print(stats[k].shape)
+            if k != 'example_state':
+                print(stats[k])
