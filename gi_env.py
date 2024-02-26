c2a4b87fb3d52891cf5d167942cb0b1b9f4c683b
Alex Beng <599267065@qq.com>
2024-02-26 22:40:15 +0800

    update gi env

--- gi_env.py
+++ gi_env.py
@@ -3,4 +3,35 @@
 # so the 'sim' just for visualization & testing infering speed & etc.
 # real is with GI iteration
 
+# JUST for aligning with act 
 
+from constants import TASK_CONFIG
+from utils import load_data_test
+
+class GIDataEnv:
+    def __init__(self, config) -> None:
+        self.task_name = config['task_name']
+        task_config = TASK_CONFIG[self.task_name]
+        # get task parameters
+        self.dataset_dir = task_config['dataset_dir']
+        self.num_episodes = task_config['num_episodes']
+        self.camera_names = task_config['camera_names']
+
+        # 读取数据集
+        self.dataloader, self.norm_stats = load_data_test(self.dataset_dir, self.num_episodes, self.camera_names, 1)
+        print(len(self.dataloader))
+
+    def reset(self):
+        
+        pass
+
+    def render(self):
+        pass
+
+if __name__ == '__main__':
+    config = {
+        'task_name': 'nazuchi_beach_friendship',
+    }
+
+    env = GIDataEnv(config)
+    
