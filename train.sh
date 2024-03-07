114a3232be903fb4e03c40031a42c52762587f91
Alex Beng <599267065@qq.com>
2024-03-07 17:21:36 +0800

    set chunk size to 40

--- train.sh
+++ train.sh
@@ -1,13 +1,6 @@
 python3 act_imitate_learning.py \
---task_name test \
---ckpt_dir ./models \
---kl_weight 10 --chunk_size 100 --hidden_dim 512 --batch_size 8 --dim_feedforward 3200 \
---num_epochs 2000  --lr 1e-5 \
---seed 0
-
-python3 act_imitate_learning.py \
 --task_name nazuchi_beach_friendship \
 --ckpt_dir ./models \
---kl_weight 10 --chunk_size 100 --hidden_dim 512 --batch_size 4 --dim_feedforward 3200 \
+--kl_weight 10 --chunk_size 40 --hidden_dim 512 --batch_size 4 --dim_feedforward 3200 \
 --num_epochs 2000  --lr 1e-5 \
 --seed 0
