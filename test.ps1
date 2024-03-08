cf9245be774471e98af1082c459f8c55045528d4
Alex Beng <599267065@qq.com>
2024-03-09 00:39:42 +0800

    add train script in win

---
+++ test.ps1
@@ -0,0 +1,6 @@
+python act_infer.py `
+--onscreen_render `
+--ckpt_dir ./models --ckpt_name policy_best.ckpt `
+--task_name nazuchi_beach_friendship `
+--seed 1000 `
+--chunk_size 40 --hidden_dim 512 --dim_feedforward 3200 
