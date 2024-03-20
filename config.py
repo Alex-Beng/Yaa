a0b638b4c6b85816323eb09647924211db1f5f16
Alex Beng <599267065@qq.com>
2024-03-21 02:50:54 +0800

    :construction: update config

--- config.py
+++ config.py
@@ -28,17 +28,17 @@ __act_config = {
     'device': 'cuda',
 
     'policy_class': 'act',  # 'mlp' or 'act for now
-    'ckpt_dir': os.path.join(os.path.dirname(__file__), 'models/act'),
-    'pretrained': False,
-    'pretrained_ckpt': None,
+    'ckpt_dir': os.path.join(os.path.dirname(__file__), 'models/act_mse_mouse'),
+    'pretrained': True,
+    'pretrained_ckpt': r'./models/act_after6k/policy_epoch_2300_seed_0.ckpt',
     'task_name': 'nazuchi_beach_friendship',
-    'batch_size': 4,
+    'batch_size': 8,
     'seed': 0,
-    'num_epochs': 2000,
+    'num_epochs': 4000,
     'lr': 1e-4,
     'kl_weight': 100,
-    'chunk_size': 20,
-    'hidden_dim': 64,
+    'chunk_size': 40,
+    'hidden_dim': 640,
     'dim_feedforward': 1280,
 
     'lr_backbone': 1e-5,
@@ -55,8 +55,10 @@ __mlp_infer_config = {**__mlp_config, **{
 }}
 
 __act_infer_config = {**__act_config, **{
+    'ckpt_dir': os.path.join(os.path.dirname(__file__), 'models/act_after6k'),
     'seed': 1000,
-    'ckpt_name': 'policy_best.ckpt',
+    # 'ckpt_name': 'policy_best.ckpt',
+    'ckpt_name': 'policy_epoch_2300_seed_0.ckpt',
     'temporal_agg': True,
     # 'onscreen_render': True,
     # 'save_video': True,
